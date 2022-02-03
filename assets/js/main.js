1b4fb4710e918b54ccd057d4fb69c6aa3afe14ce
misaraadel <41232116+misaraadel@users.noreply.github.com>
2022-02-03 19:32:33 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,127 @@
+$(document).ready(function () {
+    window.addEventListener("load", function () {
+        const loader = document.querySelector(".loader");
+        loader.className += " hidden";
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 700) {
+            $(".scroll-top-button").fadeIn();
+        } else {
+            $(".scroll-top-button").fadeOut();
+        }
+    });
+
+    $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".navbar-fixed-top");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+    });
+
+    wow = new WOW(
+        {
+        boxClass:     'wow',     
+        animateClass: 'animated',
+        offset:       0,          
+        mobile:       true,      
+        live:         true        
+      }
+      )
+    wow.init();
+
+    $(".scroll-top-button").on('click', function () {
+        $('html , body').animate({
+            scrollTop: 0
+        }, 100);
+    });
+
+    $('.hamburger').click(function(){
+        $('.hamburger').toggleClass('active');
+        $('.navbar-nav').toggleClass('acitve-nav');
+        $('body').toggleClass('overflowNone')
+    });
+
+    $('.owl-header').owlCarousel({
+        rtl: true,
+        loop:true,
+        margin: 20,
+        nav:false,
+        dots: true,
+        autoplay: false,
+        touchDrag  :true,
+		mouseDrag  : true,
+		autoWidth: false,
+		animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplayTimeout: 2000,
+		smartSpeed: 2000,
+		dragEndSpeed: 2000,
+		slidSpeed: 900,
+        paginationSpeed: 900,
+        autoplayHoverPause: true,
+        items:1
+    });
+
+    $('.owl-category').owlCarousel({
+        rtl: true,
+        loop:true,
+        margin: 20,
+        nav:false,
+        dots: true,
+        autoplay: true,
+        touchDrag  : true,
+		mouseDrag  : true,
+		autoWidth: false,
+		animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplayTimeout: 6000,
+		smartSpeed: 2000,
+		dragEndSpeed: 2000,
+		slidSpeed: 900,
+        paginationSpeed: 900,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+     $('.owl-products').owlCarousel({
+        rtl: true,
+        loop:true,
+        margin: 20,
+        nav:false,
+        dots: true,
+        autoplay: true,
+        touchDrag  : true,
+		mouseDrag  : true,
+		autoWidth: false,
+		animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplayTimeout: 6000,
+		smartSpeed: 2000,
+		dragEndSpeed: 2000,
+		slidSpeed: 900,
+        paginationSpeed: 900,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+});
