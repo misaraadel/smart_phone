8b0f408c825bd7ea19ab2036b6da5a33fefe307b
misaraadel <41232116+misaraadel@users.noreply.github.com>
2022-02-07 02:31:37 +0200

    end some of pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,24 @@ $(document).ready(function () {
         $('body').toggleClass('overflowNone')
     });
 
+    $("#slider-range").slider({
+        range:true,
+        rtl: true,
+        orientation:"horizontal",
+        min: 0,
+        max: 10000,
+        values: [0, 10000],
+        step: 100,
+        slide:function (event, ui) {
+            if (ui.values[0] == ui.values[1]) {
+            return false;
+            }     
+            $("#min_price").val(ui.values[0]);
+            $("#max_price").val(ui.values[1]);
+
+        }
+    });
+
     $('.owl-header').owlCarousel({
         rtl: true,
         loop:true,
